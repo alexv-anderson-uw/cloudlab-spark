edefa3fafae3ff27eb27421e4bd10c62e8b11309
Alex Anderson <aanderson65@wisc.edu>
2020-09-04 16:09:29 -0500

    Despatate attempt to install Hadoop

--- hadoop.bash
+++ hadoop.bash
@@ -4,7 +4,7 @@ sudo apt-get update --fix-missing
 sudo apt-get --yes install openjdk-8-jdk
 
 wget http://apache.mirrors.hoobly.com/hadoop/common/hadoop-3.1.4/hadoop-3.1.4.tar.gz -O ~/hadoop-3.1.4.tar.gz
-tar zvxf ~/hadoop-3.1.4.tar.gz
+tar zvxf ~/hadoop-3.1.4.tar.gz -C ~
 rm ~/hadoop-3.1.4.tar.gz
 
 mkdir /dev/shm/hadoop/
@@ -21,8 +21,8 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
     <name>dfs.datanode.data.dir</name>
     <value>/dev/shm/hadoop/data/</value>
   </property>
-</configuration>' > hadoop-3.1.4/etc/hadoop/hdfs-site.xml
+</configuration>' > ~/hadoop-3.1.4/etc/hadoop/hdfs-site.xml
 
-sed -i -e 's+# export JAVA_HOME=+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre+g' ./hadoop-3.1.4/etc/hadoop/hadoop-env.sh
+sed -i -e 's+# export JAVA_HOME=+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre+g' ~/hadoop-3.1.4/etc/hadoop/hadoop-env.sh
 
 echo 'export PATH="$PATH:~/hadoop-3.1.4/bin:~/hadoop-3.1.4/sbin"' >> ~/.profile
